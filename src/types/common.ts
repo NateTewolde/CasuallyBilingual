3fda4f4f87781d631bba30e45e3f90e88a3ce449
NateTewolde <tewolde.nate@gmail.com>
2023-04-07 16:02:32 -0700

    Add interface for handleUpdateClick

--- src/types/common.ts
+++ src/types/common.ts
@@ -1,23 +1,29 @@
 export interface OptionInfo {
-  option: string
-  optionType: string
-  optionVal: string
-  handleOptionChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+  option: string;
+  optionType: string;
+  optionVal: string;
+  handleOptionChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 export interface OptionsArray {
-  optionsArray: OptionInfo[]
+  optionsArray: OptionInfo[];
 }
 
 export interface OptionProp {
-  optionInfo: OptionInfo
+  optionInfo: OptionInfo;
 }
 
 export interface TranslationInfo {
-  percent: number
-  languageFrom: string
-  languageTo: string
+  percent: number;
+  languageFrom: string;
+  languageTo: string;
 }
 
 export interface TranslationDescProp {
-  translateInfo: TranslationInfo
+  translateInfo: TranslationInfo;
+}
+
+export interface UpdateOptionsBtnProp {
+  handleUpdateClick: (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => void;
 }
