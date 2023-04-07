47e4c75f0cc0db1b946156ce220cb45fb671dcc7
NateTewolde <tewolde.nate@gmail.com>
2023-04-07 16:07:45 -0700

    Update translation states on button click

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,19 +1,58 @@
-import React, { useState } from "react"
-
-import ActiveToggle from "~components/ActiveToggle"
-import ConfigOptions from "~components/ConfigOptions"
-import Title from "~components/Title"
-import TranslationDesc from "~components/TranslationDesc"
-import UpdateOptionsBtn from "~components/UpdateOptionsBtn"
-import type { OptionInfo, TranslationInfo } from "~types/common"
+import React, { useRef, useState, ChangeEvent } from "react";
+import ActiveToggle from "~components/ActiveToggle";
+import ConfigOptions from "~components/ConfigOptions";
+import Title from "~components/Title";
+import TranslationDesc from "~components/TranslationDesc";
+import UpdateOptionsBtn from "~components/UpdateOptionsBtn";
+import type { OptionInfo, TranslationInfo } from "~types/common";
 
 function IndexPopup() {
-  //const [data, setData] = useState("")
-  //  <input onChange={(e) => setData(e.target.value)} value={data} />
+  const [percent, setPercent] = useState(15);
+  const [languageFrom, setLanguageFrom] = useState(`English`);
+  const [languageTo, setLanguageTo] = useState("Arabic");
 
-  const [percent, setPercent] = useState(15)
-  const [languageFrom, setLanguageFrom] = useState(`English`)
-  const [languageTo, setLanguageTo] = useState("Arabic")
+  const optionRefs = {
+    percentRef: useRef({ optionLabel: "percent", optionValue: percent }),
+    languageFromRef: useRef({
+      optionLabel: "languageFrom",
+      optionValue: languageFrom,
+    }),
+    languageToRef: useRef({
+      optionLabel: "languageTo",
+      optionValue: languageTo,
+    }),
+  };
+
+  const handleOptionChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const label = `${e.target.dataset.label}`;
+
+    if (label === "percent") {
+      optionRefs.percentRef.current = {
+        optionLabel: "percent",
+        optionValue: Number(e.target.value),
+      };
+    }
+    if (label === "languageFrom") {
+      optionRefs.languageFromRef.current = {
+        optionLabel: "languageFrom",
+        optionValue: e.target.value,
+      };
+    }
+    if (label === "languageTo") {
+      optionRefs.languageToRef.current = {
+        optionLabel: "languageTo",
+        optionValue: e.target.value,
+      };
+    }
+  };
+
+  const handleUpdateClick = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    setPercent(optionRefs.percentRef.current.optionValue);
+    setLanguageFrom(optionRefs.languageFromRef.current.optionValue);
+    setLanguageTo(optionRefs.languageToRef.current.optionValue);
+  };
 
   const optionsArray: OptionInfo[] = [
     {
@@ -21,42 +60,43 @@ function IndexPopup() {
       optionType: "percent",
       optionVal: `${percent}`,
       handleOptionChange: (e: React.ChangeEvent<HTMLInputElement>) =>
-        handleOptionChange(e)
+        handleOptionChange(e),
     },
     {
       option: "From?",
       optionType: "languageFrom",
       optionVal: languageFrom,
       handleOptionChange: (e: React.ChangeEvent<HTMLInputElement>) =>
-        handleOptionChange(e)
+        handleOptionChange(e),
     },
     {
       option: "To?",
       optionType: "languageTo",
       optionVal: languageTo,
       handleOptionChange: (e: React.ChangeEvent<HTMLInputElement>) =>
-        handleOptionChange(e)
-    }
-  ]
-
-  const handleOptionChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    console.log(e.target.dataset.label)
-  }
+        handleOptionChange(e),
+    },
+  ];
 
   return (
     <div
       style={{
         display: "flex",
         flexDirection: "column",
-        padding: 16
-      }}>
+        padding: 16,
+      }}
+    >
       <Title />
       <ActiveToggle />
       <TranslationDesc translateInfo={{ percent, languageFrom, languageTo }} />
       <ConfigOptions optionsArray={optionsArray} />
-      <UpdateOptionsBtn />
+      <UpdateOptionsBtn
+        handleUpdateClick={(
+          e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+        ) => handleUpdateClick(e)}
+      />
     </div>
-  )
+  );
 }
 
-export default IndexPopup
+export default IndexPopup;
