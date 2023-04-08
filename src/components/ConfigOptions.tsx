ba582b224d0ff9ca7cd9e4d514860e2fe1c138b9
NateTewolde <tewolde.nate@gmail.com>
2023-04-07 17:56:41 -0700

    Import and use uniqid for unique keys

--- src/components/ConfigOptions.tsx
+++ src/components/ConfigOptions.tsx
@@ -1,8 +1,7 @@
-import React from "react"
-
-import type { OptionInfo, OptionsArray } from "~types/common"
-
-import Option from "./Option"
+import Option from "./Option";
+import React from "react";
+import uniqid from "uniqid";
+import type { OptionInfo, OptionsArray } from "~types/common";
 
 //Takes in a list of options as a prop to display.
 const ConfigOptions = ({ optionsArray }: OptionsArray) => {
@@ -11,11 +10,11 @@ const ConfigOptions = ({ optionsArray }: OptionsArray) => {
       <button>+Options</button>
       <ul>
         {optionsArray.map((option) => {
-          return <Option optionInfo={option} />
+          return <Option optionInfo={option} key={uniqid()} />;
         })}
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default ConfigOptions
+export default ConfigOptions;
