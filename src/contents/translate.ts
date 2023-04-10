637aaef951d019907e4339b088067c8d15c1ccc6
NateTewolde <tewolde.nate@gmail.com>
2023-04-09 20:14:06 -0700

    Update translateWord to communicate with background script to translate given word

--- src/contents/translate.ts
+++ src/contents/translate.ts
@@ -1,11 +1,21 @@
-import changeWords from "./change-words";
-
-const translatePage = (
-  percent: string,
-  languageFrom: string,
-  languageTo: string
-) => {
-  changeWords(Number(percent));
+const translateWord = (
+  text: string,
+  from: string,
+  to: string
+): Promise<string> => {
+  return new Promise((resolve, reject) => {
+    chrome.runtime.sendMessage(
+      { action: "translate", text: text, from: from, to: to },
+      (response) => {
+        if (response.error) {
+          console.error(response.error);
+          resolve(text);
+        } else {
+          resolve(response.translatedText);
+        }
+      }
+    );
+  });
 };
 
-export default translatePage;
+export default translateWord;
