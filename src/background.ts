fbc1cef0e33f7ce9370b7eccd8f97a7b52b65e92
NateTewolde <tewolde.nate@gmail.com>
2023-04-09 21:30:53 -0700

    Add more information to error and split apart batching into chunks to prevent rate issues

--- src/background.ts
+++ src/background.ts
@@ -17,9 +17,35 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 
 async function translateText(text: string, from: string, to: string) {
   try {
-    const res = await translate(text, { from: from, to: to });
-    return res.text;
+    if (!text || text.trim().length === 0) {
+      console.log(
+        "Empty or whitespace-only text provided. Skipping translation."
+      );
+      return;
+    }
+
+    //splits text into chunks
+    const chunkSize = 1000;
+    const textChunks = [];
+    for (let i = 0; i < text.length; i += chunkSize) {
+      textChunks.push(text.slice(i, i + chunkSize));
+    }
+
+    //translates each chunk and join the results
+    const translatedChunks = [];
+    for (const chunk of textChunks) {
+      const res = await translate(chunk, { from: from, to: to });
+      translatedChunks.push(res.text);
+    }
+    const translatedText = translatedChunks.join("");
+
+    return translatedText;
   } catch (err) {
-    console.error(err);
+    console.error("Error while translating text:", {
+      text: text,
+      from: from,
+      to: to,
+      error: err,
+    });
   }
 }
