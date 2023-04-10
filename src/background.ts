1fb46fb31579872ba67229e0618acceef7c3d949
NateTewolde <tewolde.nate@gmail.com>
2023-04-09 20:09:08 -0700

    Add eventlistener and translateText() for API calls in background to bypass CORS restrictions.

--- src/background.ts
+++ src/background.ts
@@ -1,2 +1,25 @@
-export {}
-console.log("HELLO WORLD FROM BGSCRIPTS")
+export {};
+const translate = require("google-translate-api-x");
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action === "translate") {
+    translateText(request.text, request.from, request.to)
+      .then((translatedText) => {
+        sendResponse({ translatedText: translatedText });
+      })
+      .catch((error) => {
+        sendResponse({ error: error });
+      });
+
+    return true;
+  }
+});
+
+async function translateText(text: string, from: string, to: string) {
+  try {
+    const res = await translate(text, { from: from, to: to });
+    return res.text;
+  } catch (err) {
+    console.error(err);
+  }
+}
