f0a0b0241e29703ac75e074f46460553a14e71ec
NateTewolde <tewolde.nate@gmail.com>
2023-04-11 15:44:10 -0700

    Update applyColorToSpans call to setTextColor for better understanding

--- src/contents/content.ts
+++ src/contents/content.ts
@@ -1,4 +1,4 @@
-import { addSpan, applyColorToSpans, translateSpanWords } from "./change-words";
+import { addSpan, setTextColor, translateSpanWords } from "./change-words";
 import { Storage } from "@plasmohq/storage";
 
 const storage = new Storage();
@@ -15,7 +15,7 @@ async function main() {
   const { percent, languageFrom, languageTo } = await getOptionValues();
   addSpan(Number(percent));
   translateSpanWords(languageFrom, languageTo);
-  applyColorToSpans("green");
+  setTextColor("green");
 }
 
 window.addEventListener("load", main);
