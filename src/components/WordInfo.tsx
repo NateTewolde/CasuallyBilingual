4e58596f02e1c081a2484c1b6edf32c1ba7d6122
NateTewolde <tewolde.nate@gmail.com>
2023-04-12 10:11:56 -0700

    Add basic styling to component

--- src/components/WordInfo.tsx
+++ src/components/WordInfo.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 import type { WordInfoProp } from "~types/common";
 
-const WordInfo = ({ word }: WordInfoProp) => {
+const WordInfo = ({ content }: WordInfoProp) => {
   return (
-    <div>
-      <p>{word}</p>
+    <div
+      className="word-info"
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        padding: 16,
+        backgroundColor: "blue",
+      }}
+    >
+      <h1>HIIIIIIII</h1>
+      <p>{content}</p>
     </div>
   );
 };
