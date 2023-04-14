8494ca8efe4332bb898693f1d67c3e2f1aeba9ef
NateTewolde <tewolde.nate@gmail.com>
2023-04-14 15:29:37 -0700

    Add translateText()moved from background script for more appropriate module

--- src/contents/text-utils.ts
+++ src/contents/text-utils.ts
@@ -1,3 +1,40 @@
+const translate = require("google-translate-api-x");
+
+async function translateText(text: string, from: string, to: string) {
+  try {
+    if (!text || text.trim().length === 0) {
+      console.log(
+        "Empty or whitespace-only text provided. Skipping translation."
+      );
+      return;
+    }
+
+    //splits text into chunks
+    const chunkSize = 1000;
+    const textChunks = [];
+    for (let i = 0; i < text.length; i += chunkSize) {
+      textChunks.push(text.slice(i, i + chunkSize));
+    }
+
+    //translates each chunk and join the results
+    const translatedChunks = [];
+    for (const chunk of textChunks) {
+      const res = await translate(chunk, { from: from, to: to });
+      translatedChunks.push(res.text);
+    }
+    const translatedText = translatedChunks.join("");
+
+    return translatedText;
+  } catch (err) {
+    console.error("Error while translating text:", {
+      text: text,
+      from: from,
+      to: to,
+      error: err,
+    });
+  }
+}
+
 const isInsideAttribute = (node: Node): boolean | null => {
   return (
     node.parentNode &&
@@ -11,4 +48,4 @@ function isWord(text: string): boolean {
   return regex.test(text); // Test if the input text matches the regular expression
 }
 
-export { isWord, isInsideAttribute };
+export { isWord, isInsideAttribute, translateText };
