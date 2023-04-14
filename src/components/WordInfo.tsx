58d92f29b74302214d516a5a58d4133b4528561c
NateTewolde <tewolde.nate@gmail.com>
2023-04-14 13:41:43 -0700

    Implement styled components to format and style text

--- src/components/WordInfo.tsx
+++ src/components/WordInfo.tsx
@@ -1,22 +1,55 @@
 import React from "react";
 import styled from "styled-components";
-import type { WordInfoProp } from "~types/common";
+import type { WordInfoProp, StyledTranslatedWordProps } from "~types/common";
 
 const StyledWordInfo = styled.div`
   display: flex;
   flex-direction: column;
-  padding: 4px 16px;
+  padding: 4px 8px;
   background-color: white;
   border: 2px solid black;
-  height: 90px;
-`;
-
-const StyledWordInfoDesc = styled.div`
-  display: flex;
 `;
 
 const StyledWordInfoHeader = styled.h1`
   font-size: 0.8rem;
+`;
+
+const StyledWordInfoDesc = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const StyledSpanBorder = styled.span`
+  width: 2px;
+  border-left: 1px solid black;
+`;
+
+const StyledOriginalDiv = styled.span`
+  border-bottom: 1px solid black;
+  border-left: 1px solid black;
+  border-radius: 2px;
+  padding: 4px;
+`;
+
+const StyledTranslatedDiv = styled.span`
+  border-bottom: 1px solid black;
+  border-right: 1px solid black;
+  border-radius: 2px;
+  padding: 4px;
+  text-align: end;
+`;
+
+const StyledLanguageHeader = styled.h2`
+  font-size: 0.6rem;
+`;
+
+const StyledOriginalWord = styled.p`
+  font-size: 0.9rem;
+`;
+
+const StyledTranslatedWord = styled.p<StyledTranslatedWordProps>`
+  font-size: 0.9rem;
+  color: ${(props) => props.textColor || "black"};
 `;
 
 const WordInfo = ({ wordInfo }: WordInfoProp) => {
@@ -25,14 +58,17 @@ const WordInfo = ({ wordInfo }: WordInfoProp) => {
     <StyledWordInfo className="word-info">
       <StyledWordInfoHeader>Casually Bilingual</StyledWordInfoHeader>
       <StyledWordInfoDesc>
-        <div>
-          <h2>{wordInfo.languageFrom}</h2>
-          <p>{wordInfo.originalWord}</p>
-        </div>
-        <div>
-          <h2>{wordInfo.languageTo}</h2>
-          <p>{wordInfo.translatedWord}</p>
-        </div>
+        <StyledOriginalDiv>
+          <StyledLanguageHeader>{wordInfo.languageFrom}</StyledLanguageHeader>
+          <StyledOriginalWord>{wordInfo.originalWord}</StyledOriginalWord>
+        </StyledOriginalDiv>
+        <StyledSpanBorder></StyledSpanBorder>
+        <StyledTranslatedDiv>
+          <StyledLanguageHeader>{wordInfo.languageTo}</StyledLanguageHeader>
+          <StyledTranslatedWord textColor={wordInfo.textColor}>
+            {wordInfo.translatedWord}
+          </StyledTranslatedWord>
+        </StyledTranslatedDiv>
       </StyledWordInfoDesc>
     </StyledWordInfo>
   );
