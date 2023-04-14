abd53e9e75b0049c5ed9b4df05879fc095baaeae
NateTewolde <tewolde.nate@gmail.com>
2023-04-13 21:44:07 -0700

    Update prop parameter and type and implement styled components

--- src/components/WordInfo.tsx
+++ src/components/WordInfo.tsx
@@ -1,20 +1,40 @@
 import React from "react";
+import styled from "styled-components";
 import type { WordInfoProp } from "~types/common";
 
-const WordInfo = ({ content }: WordInfoProp) => {
+const StyledWordInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 4px 16px;
+  background-color: white;
+  border: 2px solid black;
+  height: 90px;
+`;
+
+const StyledWordInfoDesc = styled.div`
+  display: flex;
+`;
+
+const StyledWordInfoHeader = styled.h1`
+  font-size: 0.8rem;
+`;
+
+const WordInfo = ({ wordInfo }: WordInfoProp) => {
+  console.log(wordInfo.languageFrom);
   return (
-    <div
-      className="word-info"
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        padding: 16,
-        backgroundColor: "blue",
-      }}
-    >
-      <h1>HIIIIIIII</h1>
-      <p>{content}</p>
-    </div>
+    <StyledWordInfo className="word-info">
+      <StyledWordInfoHeader>Casually Bilingual</StyledWordInfoHeader>
+      <StyledWordInfoDesc>
+        <div>
+          <h2>{wordInfo.languageFrom}</h2>
+          <p>{wordInfo.originalWord}</p>
+        </div>
+        <div>
+          <h2>{wordInfo.languageTo}</h2>
+          <p>{wordInfo.translatedWord}</p>
+        </div>
+      </StyledWordInfoDesc>
+    </StyledWordInfo>
   );
 };
 export default WordInfo;
