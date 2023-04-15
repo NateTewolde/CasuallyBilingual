e5f98770f48b5ec63014b0a2b7bb84ee60f253ce
NateTewolde <tewolde.nate@gmail.com>
2023-04-15 15:17:02 -0700

    Utilize PlasmoCSConfig to exclude google search due to error of constant reloading

--- src/contents/content.ts
+++ src/contents/content.ts
@@ -6,6 +6,17 @@ import {
 } from "./change-words";
 import attachWordInfo from "./diplay-word-info";
 import { Storage } from "@plasmohq/storage";
+import type { PlasmoCSConfig } from "plasmo";
+
+export const config: PlasmoCSConfig = {
+  matches: ["<all_urls>"],
+  exclude_matches: [
+    "*://*.google.com/search*",
+    "*://*.google.ca/search*",
+    "*://*.google.co.uk/search*",
+  ],
+  all_frames: true,
+};
 
 const storage = new Storage();
 
