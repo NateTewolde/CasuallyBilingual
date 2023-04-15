4a5c121c45dafcd39ab08d0ecfdf25a764acf8b5
NateTewolde <tewolde.nate@gmail.com>
2023-04-15 15:16:15 -0700

    Remove un-needed console.log

--- src/background.ts
+++ src/background.ts
@@ -2,7 +2,6 @@ import { Storage } from "@plasmohq/storage";
 import { translateText } from "~contents/text-utils";
 
 export {};
-console.log("hi background");
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === "translate") {
     translateText(request.text, request.from, request.to)
