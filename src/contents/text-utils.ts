2d0e677829a3ed6da9e1e46ac8f61a3ee2d4fa05
NateTewolde <tewolde.nate@gmail.com>
2023-04-16 22:24:08 -0700

    Update translateText to also return pronunBatchText for transliterations of text

--- src/contents/text-utils.ts
+++ src/contents/text-utils.ts
@@ -1,4 +1,5 @@
-const translate = require("google-translate-api-x");
+import translate from "google-translate-api-x";
+import type { BatchTranslations } from "~types/common";
 
 async function translateText(text: string, from: string, to: string) {
   try {
@@ -16,13 +17,17 @@ async function translateText(text: string, from: string, to: string) {
     }
 
     const translatedChunks = [];
+    const pronunChunks = [];
     for (const chunk of textChunks) {
       const res = await translate(chunk, { from: from, to: to });
       translatedChunks.push(res.text);
+      pronunChunks.push(res.pronunciation);
     }
-    const translatedText = translatedChunks.join("");
 
-    return translatedText;
+    const translatedBatchText = translatedChunks.join("");
+    const pronunBatchText = pronunChunks.join("");
+
+    return { translatedBatchText, pronunBatchText };
   } catch (err) {
     console.error("Error while translating text:", {
       text: text,
@@ -38,14 +43,14 @@ const getTranslation = (
   text: string,
   from: string,
   to: string
-): Promise<string> => {
+): Promise<BatchTranslations> => {
   return new Promise((resolve, reject) => {
     chrome.runtime.sendMessage(
       { action: "translate", text: text, from: from, to: to },
       (response) => {
         if (response.error) {
           console.error(response.error);
-          resolve(text);
+          resolve({ translatedBatchText: text, pronunBatchText: "" });
         } else {
           resolve(response.translatedText);
         }
